d258ce7d7923c27dfabd403d1a096c952937ebfd
PedroDavid <setrpel@gmail.com>
2019-11-17 13:28:52 -0300

    Updates

---
+++ precache-manifest.7226d68d432865a306c1add6b083526f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5eff3d5d1b0f5461082c7bdd9a18b23",
+    "url": "/setrpel/taads.git/index.html"
+  },
+  {
+    "revision": "01af6f18a0c2c45d9184",
+    "url": "/setrpel/taads.git/static/css/main.59a5f386.chunk.css"
+  },
+  {
+    "revision": "ac662d34b919a6c0356b",
+    "url": "/setrpel/taads.git/static/js/2.dda5ff86.chunk.js"
+  },
+  {
+    "revision": "01af6f18a0c2c45d9184",
+    "url": "/setrpel/taads.git/static/js/main.0c4e2ec4.chunk.js"
+  },
+  {
+    "revision": "a4a1222b8e6e14b0a68e",
+    "url": "/setrpel/taads.git/static/js/runtime~main.337eb904.js"
+  }
+]);
